d676b2a6105db225bb0867b53520919659232225
John Desrosiers <johndrunner@yahoo.com>
2017-11-08 08:41:35 -0800

    added minify-js task and Freelancer file header to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 /* eslint-env node es6 */
 const cleanCSS = require('gulp-clean-css');
 const gulp = require('gulp');
-const gutil = require('gulp-util');
+const header = require('gulp-header');
+// const gutil = require('gulp-util');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
-// map css file to build
+// Set the banner content for files from Freelancer Template
+const freelancerBanner =
+`/*!
+* Start Bootstrap - Freelancer v4.0.0-beta (http://startbootstrap.com/template-overviews/freelancer)
+* Copyright 2013-2017 Start Bootstrap
+* Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+`;
+
+// minify css file
 gulp.task('minify-css', () => gulp
   .src('src/css/**/*.css')
   .pipe(cleanCSS({ compatibility: 'ie8' }))
   .pipe(rename({ suffix: '.min' }))
   .pipe(gulp.dest('build/css')));
+
+// minify javascript
+gulp.task('minify-js', () => gulp
+  .src('src/js/**/*.js')
+  .pipe(uglify())
+  .pipe(header(freelancerBanner))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('build/js')));
