77927419f75e55ae36fd53522af6170c3709b26d
John Desrosiers <johndrunner@yahoo.com>
2017-11-08 07:59:06 -0800

    added css-minify task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-// log that gulp is running
 /* eslint-env node es6 */
-let gulp = require('gulp');
-let gutil = require('gulp-util');
+const cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const rename = require('gulp-rename');
 
-gulp.task('default', () => gutil.log('Gulp is running'));
+// map css file to build
+gulp.task('minify-css', () => gulp
+  .src('src/css/**/*.css')
+  .pipe(cleanCSS({ compatibility: 'ie8' }))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('build/css')));
