981ba97c6b59b7d80e2a9274f23f062cb6727621
John Desrosiers <john.desro@gmail.com>
2018-10-06 12:53:28 -0700

    fix: Upgrade to gulp v4 to fix vulnerabilities
    Upgrade from Gulp v3 to v4. This required making changes because v4
    broke the signature for the task method. The change permitted some
    refactoring to improve readability. Also, the API required modifying
    tasks for the new asynchronous api employed by Gulp.
    
    - Build dir and contents are deleted before building when running
      `$ npm run build`.
    
    - Gulp Docs: https://gulpjs.org/
    - Quick Guid for switching to Gulp 4:
      https://codeburst.io/switching-to-gulp-4-0-271ae63530c0

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,56 @@
-const cleanCSS = require('gulp-clean-css');
-const compileHb = require('./utils/compileHandlebars.js');
-const gulp = require('gulp');
-const header = require('gulp-header');
-const rename = require('gulp-rename');
-const replace = require('gulp-replace');
-const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css')
+const gulp = require('gulp')
+const header = require('gulp-header')
+const rename = require('gulp-rename')
+const replace = require('gulp-replace')
+const uglify = require('gulp-uglify')
+const path = require('path')
+const { execSync } = require('child_process')
+const compileHbs = require('./utils/compileHandlebars.js')
+const freelancerBanner = require('./template/freelancerBanner')
 
-// Set the banner content for files from Freelancer Template
-const freelancerBanner =
-`/*!
-* Start Bootstrap - Freelancer v4.0.0-beta (http://startbootstrap.com/template-overviews/freelancer)
-* Copyright 2013-2017 Start Bootstrap
-* Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-`;
+exports.default = gulp.series(
+  clearBuild,
+  gulp.parallel(compileHbs, minifyCss, minifyJs, copyVendorFiles, copyImages),
+  updateHtmlLinks
+)
 
-const updateHtmlLinks = function updateHtmlSourceLinksAndCopyToBuild() {
-  gulp
+function clearBuild(done) {
+  const targetDir = path.join(__dirname, 'build')
+  const rmrf = `rm -rf ${targetDir}`
+  execSync(rmrf)
+  done()
+}
+
+function minifyCss() {
+  return gulp
+    .src('src/css/**/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/css'))
+}
+
+function minifyJs() {
+  return gulp
+    .src('src/js/**/*.js')
+    .pipe(uglify())
+    .pipe(header(freelancerBanner))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/js'))
+}
+
+function copyVendorFiles() {
+  return gulp.src('src/img/**').pipe(gulp.dest('build/img'))
+}
+
+function copyImages() {
+  return gulp.src('src/vendor/**').pipe(gulp.dest('build/vendor'))
+}
+
+function updateHtmlLinks() {
+  return gulp
     .src('src/index.html')
     .pipe(replace('freelancer.css', 'freelancer.min.css'))
     .pipe(replace('freelancer.js', 'freelancer.min.js'))
-    .pipe(gulp.dest('build'));
-};
-
-// minify css file
-gulp.task('minify-css', () => gulp
-  .src('src/css/**/*.css')
-  .pipe(cleanCSS({ compatibility: 'ie8' }))
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('build/css')));
-
-// minify javascript files in src/js
-gulp.task('minify-js', () => gulp
-  .src('src/js/**/*.js')
-  .pipe(uglify())
-  .pipe(header(freelancerBanner))
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('build/js')));
-
-// copy files
-gulp.task('copy', () => {
-  gulp.src('src/vendor/**').pipe(gulp.dest('build/vendor'));
-  gulp.src('src/img/**').pipe(gulp.dest('build/img'));
-});
-
-// User minified source files in index.html
-gulp.task('htmlLinks', updateHtmlLinks);
-
-// Compile hbs files to html
-gulp.task('compileHbs', compileHb);
-
-gulp.task('default', ['compileHbs', 'minify-css', 'minify-js', 'copy'], () => {
-  updateHtmlLinks();
-});
+    .pipe(gulp.dest('build'))
+}
---
+++ template/freelancerBanner.js
@@ -0,0 +1,6 @@
+// freelancer license banner as an html comment
+module.exports = `/*!
+* Start Bootstrap - Freelancer v4.0.0-beta (http://startbootstrap.com/template-overviews/freelancer)
+* Copyright 2013-2017 Start Bootstrap
+* Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+*/`
