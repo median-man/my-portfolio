888ecb72371a68b7c3fb67be558d09cc1d9d4794
John Desrosiers <john.desro@gmail.com>
2019-04-29 07:30:46 -0700

    Misc changes from months agao

--- utils/compileHandlebars.js
+++ utils/compileHandlebars.js
@@ -1,26 +1,41 @@
 /* eslint import/no-extraneous-dependencies: off */
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const handlebars = require('gulp-compile-handlebars');
-const path = require('path');
-const projects = require('../template/projects.json');
-const contactData = require('../template/contactData.json');
-
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const handlebars = require('gulp-compile-handlebars')
+const debug = require('gulp-debug')
+const path = require('path')
+const projects = require('../template/projects.json')
+const contactData = require('../template/contactData.json')
 
 // Returns the name of a description partial
 const whichDescription = function whichDescriptionPartial(name) {
-  return `descriptions/${name}`;
-};
+  return `project-descriptions/${name}`
+}
 
-module.exports = function compileHandlebars() {
+function buildProjectDescriptions() {
+  return gulp
+    .src('content/project-descriptions/*.html', { cwd: __dirname })
+    .pipe(debug({ minimal: false }))
+    .pipe(
+      gulp.dest('../template/partials/project-descriptions'),
+      { cwd: __dirname }
+    )
+}
+
+function compile() {
   const options = {
     batch: [path.join(__dirname, '../template/partials')],
-    helpers: { whichDescription },
-  };
-  const templateData = { projects, contactData };
+    helpers: { whichDescription }
+  }
+  const templateData = { projects, contactData }
   return gulp
-    .src(path.join(__dirname, '../template/index.hbs'))
+    .src('../template/index.hbs', { cwd: __dirname })
     .pipe(handlebars(templateData, options))
     .pipe(rename('index.html'))
-    .pipe(gulp.dest(path.join(__dirname, '../src')));
-};
+    .pipe(gulp.dest(path.join(__dirname, '../src')))
+}
+
+module.exports = function compileHandlebars(done) {
+  gulp.series(buildProjectDescriptions, compile)
+  return done()
+}
